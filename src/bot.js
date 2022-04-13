012f3bb5c639746b879c0b55615f690ec1c14d28
Girop <65816493+Girop@users.noreply.github.com>
2022-04-13 12:04:07 +0200

    status

--- src/bot.js
+++ src/bot.js
@@ -6,23 +6,39 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 const token = process.env.CLIENT_TOKEN
 const guildId = process.env.GUILD_ID
 
-async function setPresence(){
-    const res = await fetch(
-        ``
-    )
+async function setPresence() {
+    const adress = 'amazenBooze.aternos.me'
+    try {
+        const res = await fetch(`https://api.mcsrvstat.us/2/${adress}`)
+        const data = await res.json() || res.text()
+        console.log(data)
+        // console.log(players['online'])
+        // const status = false
+        // client.user.setPresence({
+        //     activities: [
+        //         {
+        //             name: `${players['online']} people`,
+        //             type: 'PLAYING',
+        //         },
+        //     ],
+        //     status: status ? 'online' : 'idle',
+        // })
+    } catch (err) {
+        console.error(err)
+    }
 }
 
 client.once('ready', () => {
     console.log('----------------------')
     console.log('Collecting blackstone!')
     console.log('----------------------')
-
-    
+    // setInterval(setPresence, 10000)
+    setPresence()
 })
 
 // command handler
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return
-}
+})
 
 client.login(token)
