3bc67a8d8a7a917391a00da08e7ca55ca2392e90
Girop <65816493+Girop@users.noreply.github.com>
2022-04-13 10:37:06 +0200

    rewrite

--- src/bot.js
+++ src/bot.js
@@ -1,35 +1,28 @@
-console.log('----refresh----')
 import { Client, Intents } from 'discord.js'
 import 'dotenv/config'
 import fetch from 'node-fetch'
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 const token = process.env.CLIENT_TOKEN
+const guildId = process.env.GUILD_ID
+
+async function setPresence(){
+    const res = await fetch(
+        ``
+    )
+}
 
 client.once('ready', () => {
+    console.log('----------------------')
     console.log('Collecting blackstone!')
+    console.log('----------------------')
+
+    
 })
 
+// command handler
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return
-
-    const { commandName, } = interaction
-
-    switch (commandName) {
-        case 'check_status':
-            const adress = 'amazenBooze.aternos.me:16423'
-            const res = await fetch(
-                ` https://api.mcsrvstat.us/2/${adress}`
-            )
-            const {online} = await res.json()
-            await interaction.reply(
-                `${adress} is ${online ? 'online' : 'offline'}`
-                )
-            break
-        default:
-            await interaction.reply('Really ? Think again about it')
-            break
-    }
-})
+}
 
 client.login(token)
