2897f2fccd297354bd962508766bff4789b5b12f
unknown <pigor2400@gmail.com>
2022-04-19 20:28:42 +0200

    fucking app class

--- src/bot.js
+++ src/bot.js
@@ -2,133 +2,209 @@ import { Client, Intents } from 'discord.js'
 import 'dotenv/config'
 import fetch from 'node-fetch'
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
-const token = process.env.CLIENT_TOKEN
+class Session {
+    constructor(currentState) {
+        this.currentState = currentState
+        this.ended = false
 
-async function fetchData() {
-    console.log('fetchData: starting')
-    const adress = 'amazenBooze.aternos.me'
-    try {
-        const res = await fetch(`https://api.minetools.eu/ping/${adress}`)
-        const data = await res.json()
-        data['OK'] = true
-        return data
-    } catch (err) {
-        console.error(err)
-        return null
+        this.end = null
+        this.start = null
     }
-}
-async function initStatus(channel) {
-    console.log('initStatus: starting')
-    const data = await fetchData()
-    if (!data['OK']) {
-        console.log('initStatus: failed')
-        return
-    }
-    console.log('initStatus: Success')
-    setPresence(data)
-    const statusEmbed = generateStatusEmbed(data)
-    const msg = await sendEmbed(statusEmbed, channel)
-    startTicker(msg)
-}
 
-async function updateStatus(msg) {
-    console.log('Updating...')
-    const data = await fetchData()
-    if (!data['OK']) {
-        console.log('updateStatus: failed')
-        return
-    }
-    setPresence(data)
-    const statusEmbed = generateStatusEmbed(data)
-    editEmbed(msg, statusEmbed)
-}
-
-function setPresence(data) {
-    console.log('setPresence: starting')
-    const { players } = data
-    client.user.setPresence({
-        activities: [
-            {
-                name: `${players['online']} people`,
-                type: 'PLAYING',
-            },
-        ],
-        status: players['online'] > 0 ? 'online' : 'idle',
-    })
-}
-
-function generateStatusEmbed(data) {
-    const { players, version } = data
-    const status = version['name'] === '1.18.2' ? 'Online' : 'Offline'
-
-    const statusEmbed = {
-        title: 'Server info',
-        color: '#008369',
-        fields: [
-            {
-                name: 'Server:',
-                value: status,
-            },
-            {
-                name: 'People online:',
-                value: players['online'].toString(),
-            },
-            {
-                name: 'Recently',
-                value: `{start} - {end} : {maxPlayersRecentSession}`
-            },
-            {
-                name: 'Last session',
-                value: `{} - {} : {maxPlayersLastSession}`
+    update(newState, peopleOnline) {
+        this.maxPeopleOnline = Math.max(peopleOnline, this.maxPeopleOnline)
+        this.continues = this.currentState === 'Online'
+        if (newState === 'Offline') {
+            if (this.currentState === 'Online') {
+                this.end = new Date()
+                this.ended = true
             }
-        ],
-        timestamp: new Date(),
+        } else if (newState === 'Online') {
+            if (this.currentState === 'Offline') {
+                this.start = new Date()
+            }
+        }
     }
-    return statusEmbed
-}
 
-async function sendEmbed(statusEmbed, channel) {
-    console.log('Sending')
-    let msg = await channel.send({ embeds: [statusEmbed] })
-    return msg
-    // edit embed || create embed
-}
-
-function editEmbed(msg, statusEmbed) {
-    msg.edit({ embeds: [statusEmbed] })
-}
-
-function startTicker(msg) {
-    console.log('Ticking')
-    let refreshRate = 30000
-    var intervalID = setInterval(updateStatus, refreshRate, msg)
-}
-
-client.once('ready', () => {
-    console.log('----------------------')
-    console.log('Collecting blackstone!')
-    console.log('----------------------')
-})
-
-// command handler
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return
-
-    const { commandName, channel } = interaction
-
-    switch (commandName) {
-        case 'check':
-            initStatus(channel)
-            interaction.reply('Checking...')
-            break
-        // case 'summon':
-        //     await interaction.reply(client.)
-        //     break
-        default:
-            await interaction.reply('Think about it again')
-            break
+    copied() {
+        const newSession = new Session()
+        newSession.start = this.start
+        newSession.end = this.end
+        newSession.ended = this.ended
+        newSession.currentState = this.currentState
+        newSession.continues = this.continues
+        newSession.maxPeopleOnline = this.maxPeopleOnline
+        return newSession
     }
-})
 
-client.login(token)
+    toFormatedString() {
+        return `${this.start ?? ''} - ${this.end ?? ''} : ${
+            isNaN(this.maxPeopleOnline) ? '' : this.maxPeopleOnline
+        }`
+    }
+}
+class Embed {
+    constructor(data) {
+        this.color = '#008369'
+        this.recentSession = new Session()
+        this.lastSession = new Session()
+        this.update(data)
+    }
+
+    update(data) {
+        const { players, version } = data
+        let status = version['name'] === '1.18.2' ? 'Online' : 'Offline'
+        this.recentSession.update(status, players['online'])
+        this.lastSession.update(status, players['online'])
+
+        if (status === 'Online') {
+            if (this.status === 'Offline') {
+                // moving session up in a queue
+                this.recentSession = this.lastSession.copied()
+                this.lastSession = new Session(data)
+            }
+        }
+
+        this.status = status
+        this.color = this.status === 'Online' ? '#008369' : '#ff0000'
+        this.playersOnline = players['online']
+    }
+
+    getEmbedObj() {
+        const statusEmbed = {
+            title: 'Server info',
+            color: this.color,
+            fields: [
+                {
+                    name: 'Server:',
+                    value: this.status,
+                },
+                {
+                    name: 'People online:',
+                    value: this.playersOnline.toString(),
+                },
+                {
+                    name: 'Recently',
+                    value: this.recentSession.toFormatedString(),
+                },
+                {
+                    name: 'Last session',
+                    value: this.lastSession.toFormatedString(),
+                },
+            ],
+            timestamp: new Date(),
+        }
+        return statusEmbed
+    }
+}
+class App {
+    constructor() {
+        this.client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+        this.token = process.env.CLIENT_TOKEN
+        this.refreshRate = 10000
+        this.live()
+    }
+
+    live() {
+        this.client.once('ready', () => {
+            console.log('----------------------')
+            console.log('Collecting blackstone!')
+            console.log('----------------------')
+        })
+
+        // command handler
+        this.client.on('interactionCreate', async interaction => {
+            if (!interaction.isCommand()) return
+
+            const { commandName, channel } = interaction
+
+            switch (commandName) {
+                case 'check':
+                    interaction.reply('Checking...')
+                    this.initStatus(channel)
+                    break
+                default:
+                    await interaction.reply('Think about it again')
+                    break
+            }
+        })
+        this.client.login(this.token)
+        console.log('live:', this.client)
+    }
+
+    static async fetchData() {
+        console.log('fetchData: starting')
+        const adress = 'amazenBooze.aternos.me'
+        try {
+            const res = await fetch(`https://api.minetools.eu/ping/${adress}`)
+            const data = await res.json()
+            data['OK'] = true
+            console.log('fetchData: finishing')
+            return data
+        } catch (err) {
+            console.error(err)
+            return null
+        }
+    }
+    async initStatus(channel) {
+        console.log('initStatus: starting')
+        console.log('client on initStatus:', this.client)
+        const data = await App.fetchData()
+        if (!data['OK']) {
+            console.log('initStatus: failed')
+            return
+        }
+
+        App.setPresence(this.client, data)
+        this.embed = new Embed(data)
+        this.embed.update(data)
+        const statusEmbed = this.embed.getEmbedObj()
+        const msg = await this.sendEmbed(statusEmbed, channel)
+        this.startTicker(msg)
+    }
+
+    async updateStatus(msg) {
+        console.log('Updating...')
+        const data = await App.fetchData()
+        if (!data['OK']) {
+            console.log('updateStatus: failed')
+            return
+        }
+        App.setPresence(this.client, data)
+        this.embed.update(data)
+        const statusEmbed = this.embed.getEmbedObj()
+        this.editEmbed(msg, statusEmbed)
+    }
+
+    static setPresence(client, data) {
+        console.log('setPresence', thisclient)
+        console.log('setPresence: starting')
+        const { players } = data
+        client.user.setPresence({
+            activities: [
+                {
+                    name: `${players['online']} people`,
+                    type: 'PLAYING',
+                },
+            ],
+            status: players['online'] > 0 ? 'online' : 'idle',
+        })
+    }
+
+    async sendEmbed(statusEmbed, channel) {
+        console.log('Sending')
+        let msg = await channel.send({ embeds: [statusEmbed] })
+        return msg
+    }
+
+    editEmbed(msg, statusEmbed) {
+        msg.edit({ embeds: [statusEmbed] })
+    }
+
+    startTicker(msg) {
+        console.log('Ticking')
+        var intervalID = setInterval(this.updateStatus, this.refreshRate, msg)
+    }
+}
+
+const app = new App()
--- src/test.js
+++ src/test.js
@@ -1,7 +1,1 @@
-let channel = -1
-let msg = "a"
-setInterval(fetchData, 1000, msg, channel); 
-
-function fetchData(msg1, channel1){
-    console.log("fuck you", msg1, channel1);
-}
+console.log(`${null}`)
